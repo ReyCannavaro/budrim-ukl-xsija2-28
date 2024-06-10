cf86ebc60d51463b8a69ccda2a8549b5d7b9e751
ReyCannavaro <reyjunoalcannavaro@gmail.com>
2024-06-10 15:11:29 +0700

    pesan commit

--- admin/insert.php
+++ admin/insert.php
@@ -1,55 +1,61 @@
 <?php
 
-require_once 'connect.php';
-
+require_once '../admin/connect.php';
 require_once 'header.php';
 
-?>
-<div class="container">
-    <?php
+echo "<div class='container'>";
 
-    if (isset($_POST['addnew'])) {;
+// Menambah User
+if (isset($_POST['add_user'])) {
+    $username = $_POST['username'];
+    $nama = $_POST['nama'];
+    $password = $_POST['password'];
+    $level = $_POST['level'];
+    $no_telp = $_POST['no_telp'];
+    $alamat = $_POST['alamat'];
 
-        if (empty($_POST['username']) || empty($_POST['nama']) || empty($_POST['password']) || empty($_POST['domisili'])) {
-            echo "Please fillout all required fields";
-        } else {
-            $firstname  = $_POST['username'];
-            $lastname   = $_POST['nama'];
-            $address    = $_POST['password'];
-            $contact    = $_POST['domisili'];
-            $sql = "INSERT INTO user(username,nama,password,domisili) 
-            VALUES('$firstname','$lastname','$address','$contact')";
-
-            if ($con->query($sql) === TRUE) {
-                echo "<div class='alert alert-success'>Successfully added new user</div>";
-            } else {
-                echo "<div class='alert alert-danger'>Error: There was an error while adding new user</div>";
-            }
-        }
+    $sql = "INSERT INTO user (username, nama, password, level, no_telp, alamat) VALUES ('$username', '$nama', '$password', '$level', '$no_telp', '$alamat')";
+    if ($con->query($sql) === TRUE) {
+        echo "<div class='alert alert-success'>Successfully added new user</div>";
+    } else {
+        echo "<div class='alert alert-danger'>Error adding user: " . $con->error . "</div>";
     }
-    ?>
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="box">
-                <h3><i class="glyphicon glyphicon-plus"></i>&nbsp;Add New User</h3>
-                <form action="" method="POST">
-                    <label for="firstname">username</label>
-                    <input type="text" id="firstname" name="firstname" class="form-control"><br>
-                    <label for="lastname">Nama</label>
-                    <input type="text" id="lastname" name="lastname" class="form-control"><br>
-                    <label for="address">Password</label>
-                    <textarea rows="4" name="address" class="form-control"></textarea><br>
-                    <label for="contact">Domisili</label>
-                    <input type="text" id="contact" name="contact" class="form-control"><br>
-                    <br>
-                    <input type="submit" name="addnew" class="btn btn-success" value="Add New">
-                </form>
-            </div>
-        </div>
+}
+
+?>
+
+<!-- Formulir Tambah Pengguna -->
+<h2>Add New User</h2>
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" name="username" class="form-control" required>
     </div>
-</div>
+    <div class="form-group">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" name="password" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label for="level">Level:</label>
+        <input type="text" name="level" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label for="no_telp">No Telp:</label>
+        <input type="text" name="no_telp" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label for="alamat">Alamat:</label>
+        <input type="text" name="alamat" class="form-control" required>
+    </div>
+    <button type="submit" name="add_user" class="btn btn-primary">Add User</button>
+</form>
 
 <?php
+echo "</div>";
 
 require_once 'footer.php';
 ?>
